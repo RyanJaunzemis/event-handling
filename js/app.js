9ee7a6573cbcdea572a896c4588c105c33c78d14
RyanJaunzemis <khuang20@punahou.edu>
2016-07-01 14:57:16 -1000

    first commit

--- js/app.js
+++ js/app.js
@@ -1,5 +1,30 @@
 window.onload = Initiate;
 
 function Initiate(){
+	var link1 = document.getElementById("soaps");
+	link1.addEventListener('soaps', handleClick);
 
+
+	var link2 = document.getElementById("soapz");
+	link2.addEventListener('soapz', handleClick);
+
+	function handleClick(evt){
+		console.log(event.target.id);
+	}
+
+	var hoverSoap = document.getElementById("soaps");
+	hoverSoap.addEventListener("mouseover", handleHover);
+	hoverSoap.addEventListener("mouseout", releaseHover);
+
+	var hoverSoap = document.getElementById("soapz");
+	hoverSoap.addEventListener("mouseover", handleHover);
+	hoverSoap.addEventListener("mouseout", releaseHover);
+
+	function handleHover(evt){
+		evt.target.className = 'ButtonHover';
+
+	}
+
+		function releaseHover(evt){
+		evt.target.className = 'ButtonHover2';
 }
